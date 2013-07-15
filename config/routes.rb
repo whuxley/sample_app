09b231c5323759963dcb0248ac9a26fee6be787d
whuxley <gregorytandiono@me.com>
2013-07-15 06:35:42 -0700

    created pages mvc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "welcome/index"
+  get "pages/home"
+  get "pages/contact"
+  #get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
